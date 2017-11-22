2122f18a8913bc504af5694cfd869f6f9178f110
Tấn Nguyễn <ng1anh2tan3@gmail.com>
2017-11-22 13:21:35 +0700

    commit

---
+++ App-InfoJson/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_InfoJson
+{
+    public class Person
+    {
+
+        public string FullName { get; set; }
+        public string Phone { get; set; }
+        public string Gender { get; set; }
+        public DateTimeOffset? Birthday { get; set; }
+        public string Address { get; set; }
+    }
+}
